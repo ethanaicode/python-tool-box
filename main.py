5f5f0f0c60c80a271b051bae29170b4d2f1d373b
ethanaicode <cshengs@163.com>
2024-07-24 17:15:23 +0800

    more friendly tips
    optimized bg grayscale images and added options

--- main.py
+++ main.py
@@ -79,23 +79,48 @@ def generate_image():
         "#745274",
         "#5f889e",
     ]
-    bg_image = ''.join([APP_ROOT_DIR, "/assets/images/grayscale_base.png"])
+    # define background grayscale image
+    bg_image_id_select = input("请选择背景图片：默认随机选择\n1. base01\n2. base02\n")
+    if bg_image_id_select == "":
+        bg_image_id_select = random.randint(1, 2)
+    bg_image = ''.join([APP_ROOT_DIR, f"/assets/images/grayscale_base_{bg_image_id_select}.png"])
+    print(f"已选择背景图片: {bg_image}\n")
+    # define background color
     bg_color = input("请输入背景颜色：默认随机选择\n")
     if(bg_color == ""):
         bg_color = random.choice(default_bg_colors)
+    print(f"已选择背景颜色: {bg_color}\n")
+    # define main text
     main_text = input("请输入主要文本：默认为#Hello World\n")
     if(main_text == ""):
         main_text = "#Hello World"
+    print(f"已输入文本: {main_text}\n")
+    # define font size
+    font_size = get_font_size()
+    print(f"已输入字体大小: {font_size}\n")
+    # define font
     text_font = input("请输入字体：默认为苹方中\n")
     if(text_font == ""):
         text_font = ''.join([APP_ROOT_DIR, "/assets/fonts/PingFang-Mod-19.0d5e3-SC-Medium.otf"])
+    print(f"已选择字体: {text_font}\n")
+    # default output file
     output_image = ''.join([APP_ROOT_DIR, f"/output/{main_text}.png"])
 
-    command = f"magick {bg_image} +level-colors \"{bg_color},\" -gravity center -fill white -font \"{text_font}\" -pointsize 96 -annotate 0 \"{main_text}\" {output_image}"
+    # run magick command
+    command = f"magick {bg_image} +level-colors \"{bg_color},\" -gravity center -fill white -font \"{text_font}\" -pointsize {font_size} -annotate 0 \"{main_text}\" \"{output_image}\""
 
-    print(f"Running command: {command}\n")
-    run_magick_command(command)
-    print(f"Output image: {output_image}")
+    print(f"开始生成图片: {command}")
+    run_magick_command(command, is_print=False)
+    print(f"图片已生成: {output_image}")
+
+def get_font_size(default_size="96"):
+    while True:
+        text_size = input(f"请输入字体大小：默认为{default_size}\n默认配置适合中文4个字，英文8个字\n")
+        if not text_size:
+            return default_size
+        if text_size.isdigit():
+            return text_size
+        print("Error: Invalid font size.")
 
 if __name__ == "__main__":
     main()
